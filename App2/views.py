ec1be010bc17eecce244a4a6eb07f7f2f0cfc6d0
ThorV13 <joan_jmss@hotmail.com>
2023-09-25 16:18:17 -0300

    cambios en view 2

--- App2/views.py
+++ App2/views.py
@@ -7,6 +7,9 @@ from django.http import HttpResponse
 def vista2(request):
     html="""
     <h1>hola que tal x2</h1>
+    <a href=http://127.0.0.1:8000/vista3>este link conduce para vista 3</a>
+    <a href=http://127.0.0.1:8000/vista1>este link conduce para vista 1</a>
+    <a href=http://127.0.0.1:8000/vista2>este link conduce para vista 2</a>
     <a href=http://127.0.0.1:8000/vista4>este link conduce para vista 4</a>
     """
     return HttpResponse(html)
@@ -15,7 +18,10 @@ def vista2(request):
 def vista4(request):
     html="""
     <h1>hola sumaniro</h1>
+    <a href=http://127.0.0.1:8000/vista3>este link conduce para vista 3</a>
+    <a href=http://127.0.0.1:8000/vista1>este link conduce para vista 1</a>
     <a href=http://127.0.0.1:8000/vista2>este link conduce para vista 2</a>
+    <a href=http://127.0.0.1:8000/vista4>este link conduce para vista 4</a>
     """
     return HttpResponse(html)
 
