6c8838c66fb7afe6c2ae1fcdfab07792c1c901df
ThorV13 <joan_jmss@hotmail.com>
2023-09-25 15:29:23 -0300

    se crea app1 en rama1(1)

--- App1/views.py
+++ App1/views.py
@@ -4,10 +4,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def vista1(request){
+def vista1(request)
     html="""
     <h1>hola</h1>
     """
     return HttpResponse(html)
-}
+
 
