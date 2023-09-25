c13920976cb2b7fcf06e250ee20a98cf1e1a0364
ThorV13 <joan_jmss@hotmail.com>
2023-09-25 16:17:54 -0300

    cambios en view 2

--- App1/views.py
+++ App1/views.py
@@ -8,6 +8,9 @@ def vista1(request):
     html="""
     <h1>hola</h1>
     <a href=http://127.0.0.1:8000/vista3>este link conduce para vista 3</a>
+    <a href=http://127.0.0.1:8000/vista1>este link conduce para vista 1</a>
+    <a href=http://127.0.0.1:8000/vista2>este link conduce para vista 2</a>
+    <a href=http://127.0.0.1:8000/vista4>este link conduce para vista 4</a>
     """
     return HttpResponse(html)
 
@@ -15,5 +18,8 @@ def vista3(request):
     html="""
     <h1>hi my name is checkman</h1>
     <a href=http://127.0.0.1:8000/vista1>este link conduce para vista 1</a>
+    <a href=http://127.0.0.1:8000/vista2>este link conduce para vista 2</a>
+    <a href=http://127.0.0.1:8000/vista3>este link conduce para vista 3</a>
+    <a href=http://127.0.0.1:8000/vista3>este link conduce para vista 4</a>
     """
     return HttpResponse(html)
