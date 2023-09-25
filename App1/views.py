0b9ecc162848c1f1c1a238df03c38ed3d08572be
ThorV13 <joan_jmss@hotmail.com>
2023-09-25 16:31:39 -0300

    comparar

--- App1/views.py
+++ App1/views.py
@@ -6,15 +6,20 @@ from django.http import HttpResponse
 
 def vista1(request):
     html="""
-    <h1>say your name</h1>
+    <h1>hola</h1>
+    <a href=http://127.0.0.1:8000/vista3>este link conduce para vista 3</a>
+    <a href=http://127.0.0.1:8000/vista1>este link conduce para vista 1</a>
+    <a href=http://127.0.0.1:8000/vista2>este link conduce para vista 2</a>
+    <a href=http://127.0.0.1:8000/vista4>este link conduce para vista 4</a>
     """
     return HttpResponse(html)
-
 
 def vista3(request):
     html="""
     <h1>hi my name is checkman</h1>
+    <a href=http://127.0.0.1:8000/vista1>este link conduce para vista 1</a>
+    <a href=http://127.0.0.1:8000/vista2>este link conduce para vista 2</a>
+    <a href=http://127.0.0.1:8000/vista3>este link conduce para vista 3</a>
+    <a href=http://127.0.0.1:8000/vista3>este link conduce para vista 4</a>
     """
     return HttpResponse(html)
-
-
