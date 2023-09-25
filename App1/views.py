d294db16e0c0beaeda4fbb4e101bf8ae01425537
ThorV13 <joan_jmss@hotmail.com>
2023-09-25 16:01:40 -0300

    se detalla un poquito diferencia entre views

--- App1/views.py
+++ App1/views.py
@@ -6,14 +6,14 @@ from django.http import HttpResponse
 
 def vista1(request):
     html="""
-    <h1>hola</h1>
+    <h1>say your name</h1>
     """
     return HttpResponse(html)
 
 
 def vista3(request):
     html="""
-    <h1>hola</h1>
+    <h1>hi my name is checkman</h1>
     """
     return HttpResponse(html)
 
