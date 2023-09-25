eabb38b5af2d9aff548bd30e30ba6923500ab24f
ThorV13 <joan_jmss@hotmail.com>
2023-09-25 15:38:03 -0300

    se agrega html en views y se le realizan las modificaciones correspondientes

--- App2/views.py
+++ App2/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+
 # Create your views here.
+
+def vista2(request):
+    html="""
+    <h1>hola</h1>
+    """
+    return HttpResponse(html)
+
+
