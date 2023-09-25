77a1e0a639ffe6a559f9053f26936335887f74db
ThorV13 <joan_jmss@hotmail.com>
2023-09-25 15:28:49 -0300

    se crea app1 en rama1

--- App1/views.py
+++ App1/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def vista1(request){
+    html="""
+    <h1>hola</h1>
+    """
+    return HttpResponse(html)
+}
+
--- prueba1/urls.py
+++ prueba1/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from App1 import views  as version1
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('version1/', version1.vista1),
 ]
